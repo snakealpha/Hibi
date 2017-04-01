cfcf5e8778cd5fbd96fc8fb593deddc73b858b75
Snake Liu <yiyiyouxia@live.cn>
2017-04-01 17:17:12 +0800

    start automata parser

--- src/Scanner/GrammarAutomata.cs
+++ src/Scanner/GrammarAutomata.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,7 +20,7 @@ namespace Elecelf.Hibiki.Scanner
         public List<GrammarAutomata> Grammars => _grammars;
     }
 
-    public class Word
+    public class Token
     {
         public string Literal { set; get; }
         public Grammar Grammer { set; get; }
@@ -39,7 +40,25 @@ namespace Elecelf.Hibiki.Scanner
         /// <returns>Parsed automata.</returns>
         public static GrammarAutomata Parse(string rawString)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            if(string.IsNullOrEmpty(rawString))
+                throw new ParseErrorException("Raw String is null or empty.");
+
+            // Look-around 1 char
+            char? currentChar = rawString[0];
+            char? lookaroundChar = rawString.Length > 1 ? (char?)rawString[1] : null;
+            bool isInGrammarBlock = false;
+            bool isInEscapeBlock = false;
+            Queue<char?> holdingChars = new Queue<char?>();
+
+            uint lookAroundPoint = 1;
+            for (; lookAroundPoint <= rawString.Length; lookAroundPoint++)
+            {
+
+            }
+            
+            return null;
         }
     }
 
@@ -55,13 +74,13 @@ namespace Elecelf.Hibiki.Scanner
         /// <summary>
         /// Input a word to drive the state transfer to next state.
         /// </summary>
-        /// <param name="word">Word to input to current state.</param>
+        /// <param name="token">Word to input to current state.</param>
         /// <returns>Tuple: is transfer available and which state tranfering to.</returns>
-        public (bool, GrammarState) InputWord(Word word, ScannerContext context)
+        public (bool, GrammarState) InputWord(Token token, ScannerContext context)
         {
             foreach (var grammarTransfer in _transfers)
             {
-                var transferResult = grammarTransfer.InputWord(word, context);
+                var transferResult = grammarTransfer.InputWord(token, context);
                 if(transferResult.Item1)
                     return transferResult;
             }
@@ -75,35 +94,61 @@ namespace Elecelf.Hibiki.Scanner
         public TransferCondition TransferCondition { get; set; }
         public GrammarState TransfedState { get; set; }
 
-        public (bool, GrammarState) InputWord(Word word, ScannerContext context)
+        public (bool, GrammarState) InputWord(Token token, ScannerContext context)
         {
-            return (TransferCondition.Pass(word, context), TransfedState);
+            return (TransferCondition.Pass(token, context), TransfedState);
         }
     }
 
     public abstract class TransferCondition
     {
-        public abstract bool Pass(Word word, ScannerContext context);
+        public abstract bool Pass(Token word, ScannerContext context);
     }
 
     public class EpsilonTransferCondition:TransferCondition
     {
-        public override bool Pass(Word word, ScannerContext context)
+        public override bool Pass(Token token, ScannerContext context)
         {
-            if (word.Grammer.Symol == context.SymolHost.GetSymol("eps"))
+            if (token.Grammer.Symol == context.SymolHost.GetSymol("eps"))
                 return true;
 
             return false;
         }
     }
 
-    //public class StringTransferCondition : TransferCondition
-    //{
-    //    public string CompareReference { get; set; }
+    public class SymolTransferCondition : TransferCondition
+    {
+        public Symol CompareReference
+        {
+            get; set;
+        }
 
-    //    public override bool Pass(Word word, ScannerContext context)
-    //    {
-    //        return this.CompareReference == word.
-    //    }
-    //}
+        public override bool Pass(Token word, ScannerContext context)
+        {
+            return this.CompareReference == word.Grammer.Symol;
+        }
+    }
+
+    public class StringTransferCondition : TransferCondition
+    {
+        public String CompareReference
+        {
+            get; set;
+        }
+
+        public override bool Pass(Token word, ScannerContext context)
+        {
+            return this.CompareReference == word.Literal;
+        }
+    }
+
+    // Exceptions
+
+    public class ParseErrorException : Exception
+    {
+        public ParseErrorException(string info) : base(info)
+        {
+
+        }
+    }
 }
