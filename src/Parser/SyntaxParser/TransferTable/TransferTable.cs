19ba2cc770963dee7226269e998a1d84387921e7
Snake Liu <yiyiyouxia@live.cn>
2018-01-04 18:17:54 +0800

    start transfer table support

---
+++ src/Parser/SyntaxParser/TransferTable/TransferTable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Elecelf.Hibiki.Parser.SyntaxParser.TransferTable
+{
+    public class TransferTable : IParseable
+    {
+        public IState StartState { get; private set; }
+
+        public IDictionary<Symbol, IState> States { get; } = new ConcurrentDictionary<Symbol, IState>();
+
+        public IDictionary<Symbol, ITransfer> Transfers { get; } = new ConcurrentDictionary<Symbol, ITransfer>();
+    }
+}
---
+++ src/Parser/SyntaxParser/TransferTable/TransferTableState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Elecelf.Hibiki.Parser.SyntaxParser.TransferTable
+{
+    //public class TransferTableState : IState
+    //{
+    //    public bool IsTerminal
+    //    {
+    //        get;
+    //        private set;
+    //    }
+
+        
+    //}
+}
